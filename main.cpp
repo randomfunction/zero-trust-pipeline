a85aad491810954a843f9132b389cc7874266353
randomfunction <tanishqparihar3@gmail.com>
2026-03-18 21:39:44 +0530

    CrashLoopBackOff fixed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main() {
     std::cout << "Mock Trading Engine Initialized." << std::endl;
+    while(true) {
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
     return 0;
 }
